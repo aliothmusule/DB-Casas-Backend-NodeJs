8f78bdafa36312e0bd34778a19b2fad6bb544e29
samm12344 <146872828+samm12344@users.noreply.github.com>
2024-04-15 10:51:16 -0600

    Update casas.js
    documente los codigos-sam

--- models/casas.js
+++ models/casas.js
@@ -1,25 +1,25 @@
-const mysql2 = require('mysql2/promise');
-require('dotenv').config();
+const mysql2 = require('mysql2/promise'); // Utiliza MySQL con promesas para operaciones asincrónicas.
+require('dotenv').config(); // Carga variables de entorno desde un archivo .env para manejar configuraciones sin revelar datos sensibles.
 
 const config = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT,
+    host: process.env.DB_HOST, // Host de la base de datos
+    port: process.env.DB_PORT, //Puerto de la base de datos
+    database: process.env.DB_DATABASE, //Nombre de la base de datos.
+    user: process.env.DB_USER, //Usuario de la base de datos.
+    password: process.env.DB_PASSWORD, //Contraseña del usuario.
+    connectionLimit: process.env.DB_CONNECTION_LIMIT, //Límite de conexiones en el pool.
 };
 
 const pool = mysql2.createPool(config);
 // Ejemplo de modelo con consultas preparadas
 const casasModel = {
-    getAll: 'SELECT * FROM casasalioth',
-    getByID: 'SELECT * FROM casasalioth WHERE UniqueID = ?',
-    addRow: `
+    getAll: 'SELECT * FROM casasalioth', //Retorna todas las filas de la tabla.
+    getByID: 'SELECT * FROM casasalioth WHERE UniqueID = ?', // Retorna una fila específica basada en UniqueID
+    addRow: ` // Inserta una nueva fila en la tabla con múltiples campos especificados. // Inserta una nueva fila con los valores proporcionados como parámetros.
         INSERT INTO 
         casasalioth (
           
-            ParcelID, 
+            ParcelID,     
             LandUse, 
             PropertyAddress, 
             SaleDate, 
@@ -40,7 +40,7 @@ const casasModel = {
         )
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `,
-    updateByID: `
+    updateByID: ` // Actualiza una fila existente en la tabla basándose en UniqueID.
         UPDATE casasalioth 
         SET 
             ParcelID=?, 
@@ -63,10 +63,10 @@ const casasModel = {
             HalfBath=?
         WHERE UniqueID=?
     `,
-    deleteRow: 'DELETE FROM casasalioth WHERE UniqueID=?',
+    deleteRow: 'DELETE FROM casasalioth WHERE UniqueID=?', //Actualiza la fila que coincide con el UniqueID con los nuevos valores proporcionados.
 };
 
 module.exports = {
     pool,
-    casasModel,
+    casasModel, //Esto permite que otros archivos en la aplicación importen pool y casasModel para realizar operaciones de base de datos.
 };
